c88ec715c9dd672956b80eab370cff4cb0bc587a
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2013-08-14 10:45:49 +0200

    delete empty lines

--- pySDC/integrate/quadrature.py
+++ pySDC/integrate/quadrature.py
@@ -1,17 +1,14 @@
 class Quadrature:
     """
-    
     """
 
     def __init__( self ):
         """
-       
         """
         raise NotImplementedError( "Should be implemented by derivation." )
 
     @staticmethod
     def integrate():
         """
-        
         """
         raise NotImplementedError( "Should be implemented by specific scheme." )
