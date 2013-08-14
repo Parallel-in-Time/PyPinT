5a52df570d4405a0e466e2a4e0f96d0cb64082d5
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2013-08-14 09:20:09 +0200

    further tests for sanity checks of midpoint scheme

--- tests/pySDC/integrate/midpoint_tests.py
+++ tests/pySDC/integrate/midpoint_tests.py
@@ -12,8 +12,14 @@ class MidpointTests( unittest.TestCase ):
     def testIntegrate( self ):
         self.assertEqual( Midpoint.integrate(), Decimal( 1.0 ), "Default integrate values" )
         self.assertEqual( Midpoint.integrate( lambda x: Decimal( 0.0 ) ), Decimal( 0.0 ), "Zero function" )
-        with self.assertRaises( AttributeError ):
-            Midpoint.integrate( lambda x: 1.0, 0, 0, 1)
+        
+        testParams = []
+        testParams.append( [lambda x: 1.0, 0, 0, 1] )
+        testParams.append( [lambda x: 1.0, 0, 1, 0] )
+        testParams.append( [lambda x: 1.0, 1, 0, 1] )
+        for params in testParams:
+            with self.assertRaises( AttributeError ):
+                Midpoint.integrate( params[0], params[1], params[2], params[3] )
 
 if __name__ == "__main__":
     unittest.main()
