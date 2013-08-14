28f2a8cda1230dd18f0878ef209a6cedc74d22d4
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2013-08-14 11:02:09 +0200

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import setup
 
-setup( name='pySDC',
-      version='0.0.1',
-      description='A Python implementation of a Spectral Deffered Corrections (SDC) method',
-      author='Torbjörn Klatt',
-      author_email='opensource@torbjoern-klatt.de',
-      url='http://blog.torbjoern-klatt.de/',
-      packages=['pySDC'],
-     )
+def read( fname ):
+    return open( os.path.join( os.path.dirname( __file__ ), fname ) ).read()
+
+setup( name="pySDC",
+       version="0.0.1 pre-alpha",
+       author="Torbjörn Klatt",
+       author_email="opensource@torbjoern-klatt.de",
+       description=( "A Python implementation of a Spectral Deffered Corrections (SDC) method" ),
+       long_description=read( 'README' ),
+       license="MIT",
+       url="http://blog.torbjoern-klatt.de/",
+       install_requires=['numpy'],
+       packages=['pySDC', 'tests'],
+       test_suite="tests",
+       classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Environment :: Console",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.2",
+            "Topic :: Scientific/Engineering :: Mathematics"]
+ )
