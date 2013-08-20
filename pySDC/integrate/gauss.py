be7c6b28e489f6348fbbd4fc5fda806d0504a20f
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-08-20 11:07:38 +0200

    refactor Gauss integrate
    separate functions for nodes and weights

--- pySDC/integrate/gauss.py
+++ pySDC/integrate/gauss.py
@@ -20,33 +20,54 @@ class Gauss( Quadrature ):
             raise ValueError( "Integration interval must be non-zero positive (end - begin = " + str( b - a ) + ")." )
 
         transform = [ Decimal( ( b - a ) / Decimal( 2 ) ), Decimal( ( b + a ) / Decimal( 2 ) ) ]
-        pointsAndWeights = Gauss._pointsAndWeights( nPoints )
+        nodes = Gauss.nodes( nPoints )
+        weights = Gauss.weights( nPoints )
+        assert len(nodes) == len(weights), "Number of nodes must be equal to number of weights."
         result = Decimal( 0.0 )
-        for pointWeight in pointsAndWeights:
-            result += pointWeight[1] * Decimal( func( transform[0] * pointWeight[0] + transform[1] ) )
+        for i in range( 0, len(nodes) ):
+            result += weights[i] * Decimal( func( transform[0] * nodes[i] + transform[1] ) )
         result *= transform[0]
         return result
+    
+    @staticmethod
+    def nodes( nPoints ):
+        if nPoints == 3:
+            return [ Decimal( -1.0 ), 
+                     Decimal( 0.0 ), 
+                     Decimal( 1.0 ) ]
+        elif nPoints == 4:
+            return [ Decimal( -1.0 ),
+                     Decimal( -1.0 ) / Decimal( 5 ) * Decimal( 5 ).sqrt(),
+                     Decimal( 1.0 ) / Decimal( 5 ) * Decimal( 5 ).sqrt(),
+                     Decimal( 1.0 ) ]
+        elif nPoints == 5:
+            return [ Decimal( -1.0 ),
+                     Decimal( -1.0 ) / Decimal( 7 ) * Decimal( 21 ).sqrt(),
+                     Decimal( 0.0 ),
+                     Decimal( 1.0 ) / Decimal( 7 ) * Decimal( 21 ).sqrt(),
+                     Decimal( 1.0 ) ]
+        elif nPoints < 3:
+            raise ValueError( "Gauss-Lobatto quadrature does not work with less than three points." )
+        else:
+            raise NotImplementedError()
 
     @staticmethod
-    def _pointsAndWeights( nPoints ):
+    def weights( nPoints ):
         if nPoints == 3:
-            return [ [ Decimal( -1.0 ), Decimal( 1 / Decimal( 3 ) )],
-                     [ Decimal( 0.0 ), Decimal( 4 / Decimal( 3 ) )],
-                     [Decimal( 1.0 ), Decimal( 1 / Decimal( 3 ) )]
-                   ]
+            return [ Decimal( 1 / Decimal( 3 ) ),
+                     Decimal( 4 / Decimal( 3 ) ),
+                     Decimal( 1 / Decimal( 3 ) ) ]
         elif nPoints == 4:
-            return [ [ Decimal( -1.0 ), Decimal( 1 / Decimal( 6 ) ) ],
-                     [ Decimal( -1.0 ) / Decimal( 5 ) * Decimal( 5 ).sqrt(), Decimal( 5 ) / Decimal( 6 ) ],
-                     [ Decimal( 1.0 ) / Decimal( 5 ) * Decimal( 5 ).sqrt(), Decimal( 5 ) / Decimal( 6 ) ],
-                     [ Decimal( 1.0 ), Decimal( 1 / Decimal( 6 ) ) ]
-                   ]
+            return [ Decimal( 1 / Decimal( 6 ) ),
+                     Decimal( 5 ) / Decimal( 6 ),
+                     Decimal( 5 ) / Decimal( 6 ),
+                     Decimal( 1 / Decimal( 6 ) ) ]
         elif nPoints == 5:
-            return [ [ Decimal( -1.0 ), Decimal( 1 ) / Decimal( 10 ) ],
-                     [ Decimal( -1.0 ) / Decimal( 7 ) * Decimal( 21 ).sqrt(), Decimal( 49 ) / Decimal( 90 ) ],
-                     [ Decimal( 0.0 ), Decimal( 32 ) / Decimal( 45 ) ],
-                     [ Decimal( 1.0 ) / Decimal( 7 ) * Decimal( 21 ).sqrt(), Decimal( 49 ) / Decimal( 90 ) ],
-                     [ Decimal( 1.0 ), Decimal( 1 ) / Decimal( 10 ) ]
-                   ]
+            return [ Decimal( 1 ) / Decimal( 10 ),
+                     Decimal( 49 ) / Decimal( 90 ),
+                     Decimal( 32 ) / Decimal( 45 ),
+                     Decimal( 49 ) / Decimal( 90 ),
+                     Decimal( 1 ) / Decimal( 10 ) ]
         elif nPoints < 3:
             raise ValueError( "Gauss-Lobatto quadrature does not work with less than three points." )
         else:
