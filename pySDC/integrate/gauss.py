3529a42bedd51e79b6b37a184c92506776568796
Torbjörn Klatt <opensource@torbjoern-klatt.de>
2013-10-24 14:21:34 +0200

    probably PEP8-ified

--- pySDC/integrate/gauss.py
+++ pySDC/integrate/gauss.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import itertools
 
 import numpy as np
@@ -9,6 +10,7 @@ from pySDC.integrate.quadrature import Quadrature
 
 class Gauss(Quadrature):
     """
+    Provides methods to integrate with Gauss quadrature methods.
     """
 
     def __init__(self):
@@ -17,13 +19,15 @@ class Gauss(Quadrature):
         super(Quadrature, self).__init__()
 
     @staticmethod
-    def integrate(func=lambda t, x: 1.0, vals=None, begin=0, end=1, n=3, t=1.0, partial=None,
-                  method="legendre"):
+    def integrate(func=lambda t, x: 1.0, vals=None, begin=0, end=1, n=3, t=1.0,
+                  partial=None, method="legendre"):
         """
-        Integrates given function in `[begin, end]` using `nPoints` at time `t` with `method`
+        Integrates given function in `[begin, end]` using `nPoints` at time `t`
+         with `method`
 
-        :param func:    function to be integrated; requires time `t` as first and
-                        point `x` as second argument; default: constant 1 function
+        :param func:    function to be integrated; requires time `t` as first
+                        and point `x` as second argument; default: constant 1
+                        function
         :type func:     function pointer or lambda
         :param vals:    array of values to be used instead of a function
         :type vals:     array or list of Floats
@@ -37,8 +41,8 @@ class Gauss(Quadrature):
         :type t:        Integer or Float
         :param partial: index of the last value to be integrated
         :type partial:  Integer
-        :param method:  type of integration points; currently only `legendre` or
-                        `lobatto` available
+        :param method:  type of integration points; currently only `legendre`
+                        or `lobatto` available
         :type method:   String
 
         :rtype:         Float
@@ -49,8 +53,8 @@ class Gauss(Quadrature):
         _b = end
 
         if _a == _b or (_b - _a) <= 0.0:
-            raise ValueError("Integration interval must be non-zero positive (end - begin = {: f})."
-                             .format(_b - _a))
+            raise ValueError("Integration interval must be non-zero positive \
+                              (end - begin = {: f}).".format(_b - _a))
 
         _nw = {'nodes': [], 'weights': []}
         if method == "lobatto" and partial is not None:
@@ -63,50 +67,58 @@ class Gauss(Quadrature):
 
         if vals is not None:
             assert len(vals) == len(_nw['nodes']), \
-                "Number of given values ({:d}) not matching number of integration points ({:d})." \
-                .format(len(vals), len(_nw['nodes']))
+                "Number of given values ({:d}) not matching number of \
+                 integration points ({:d})." \
+                    .format(len(vals), len(_nw['nodes']))
 
         _result = {'full': 0.0, 'partial': 0.0}
         _count_terms = 0
 
         if partial is not None:
-            _smat = Gauss.build_s_matrix(_trans[0] * _nw['nodes'] + [_trans[1]] * len(_nw['nodes']),
+            _smat = Gauss.build_s_matrix(_trans[0] * _nw['nodes'] +
+                                         [_trans[1]] * len(_nw['nodes']),
                                          begin, end, method)
             Config.LOG.debug("Constructed Smat:\n{}".format(str(_smat)))
 
         if partial is None:
             for i in range(0, len(_nw['nodes'])):
                 if func is not None:
-                    _result['full'] += _nw['weights'][i] * func(t, _trans[0] * _nw['nodes'][i] + _trans[1])
+                    _result['full'] += _nw['weights'][i] * \
+                        func(t, _trans[0] * _nw['nodes'][i] + _trans[1])
                 elif vals is not None:
                     _result['full'] += _nw['weights'][i] * vals[i]
                 else:
                     raise ValueError("Either func or vals must be given.")
                 _count_terms += 1
         elif vals is not None:
-            Config.LOG.debug("using _smat row {:d}:".format(partial - 1) + str(_smat[partial - 1]))
+            Config.LOG.debug("using _smat row {:d}:".format(partial - 1) +
+                             str(_smat[partial - 1]))
             assert len(_smat[partial - 1]) == len(vals), \
-                "_smat entries ({:d}) not matching values ({:d})" \
+                "_smat entries ({:d}) not matching values ({:d})"\
                 .format(len(_smat[partial - 1]), len(vals))
             for i in range(0, len(_smat[partial - 1])):
                 _result['partial'] += _smat[partial - 1][i] * vals[i]
                 Config.LOG.debug("   {: f} += {: f} * {: f}"
-                                 .format(_result['partial'], _smat[partial - 1][i], vals[i]))
+                                 .format(_result['partial'],
+                                 _smat[partial - 1][i], vals[i]))
                 _count_terms += 1
         else:
             raise NotImplementedError("Not yet implemented")
 
-        assert _count_terms > 0, \
-            "Nothing was integrated (begin={:f}, end={:f}, n={:d}, partial={:d})." \
-                .format(begin, end, n, partial)
+        assert _count_terms > 0,\
+            "Nothing was integrated (begin={:f}, end={:f}, n={:d}, \
+             partial={:d}).".format(begin, end, n, partial)
 
         _result['full'] *= _trans[0]
         _result['partial'] *= _trans[0]
 
         if partial is not None:
-            Config.LOG.debug("integrated on [{: f},{: f}] as partial interval in [{: f}, {: f}]"
-                             .format(_trans[0] * _nw['nodes'][begin] + _trans[1],
-                                     _trans[0] * _nw['nodes'][partial] + _trans[1], begin, end))
+            Config.LOG.debug("integrated on [{: f},{: f}] as partial interval \
+                              in [{: f}, {: f}]"
+                             .format(_trans[0] * _nw['nodes'][begin] +
+                                     _trans[1],
+                                     _trans[0] * _nw['nodes'][partial] +
+                                     _trans[1], begin, end))
             Config.LOG.debug("used values: {}".format(str(vals)))
             Config.LOG.debug("n nodes: {:d}".format(_count_terms))
             return _result['partial']
@@ -116,7 +128,8 @@ class Gauss(Quadrature):
     @staticmethod
     def get_nodes_and_weights(n_points, method="legendre"):
         """
-        Returns integration nodes and weights for given type and number of points
+        Returns integration nodes and weights for given type and number of
+        points
         
         :param n_points: number of integration points
         :type n_points:  Integer
@@ -136,12 +149,14 @@ class Gauss(Quadrature):
         elif method == "lobatto":
             return Gauss.lobatto_nodes_and_weights(n_points)
         else:
-            raise NotImplementedError("Gaus-{}-Quadrature not implemented/known.".format(method))
+            raise NotImplementedError("Gaus-{}-Quadrature not implemented."
+                                      .format(method))
 
     @staticmethod
     def transform(a, b):
         """
-        Computats nodes and weights for the Gauss-Legendre quadrature of order n>1 on [-1, +1]
+        Computats nodes and weights for the Gauss-Legendre quadrature of order
+        n>1 on [-1, +1]
 
         :param a:   start of the interval
         :type a:    Float
@@ -155,7 +170,6 @@ class Gauss(Quadrature):
 
         see: http://en.wikipedia.org/wiki/Gaussian_quadrature#Change_of_interval
         """
-        #         print('[{: f}, {: f}]: {: f}, {: f}'.format(a, b, (b-a)/2.0, (b+a)/2.0))
         return [(b - a) / 2.0, (b + a) / 2.0]
 
     @staticmethod
@@ -167,7 +181,8 @@ class Gauss(Quadrature):
         :type begin:    Float
         :param end:     end of the integration interval
         :type end:      Float
-        :param method:  method of the integration nodes (either `legendre` or `lobatto`
+        :param method:  method of the integration nodes (either `legendre` or
+                        `lobatto`
         :type method:   String
 
         :rtype:
@@ -187,32 +202,34 @@ class Gauss(Quadrature):
                 smat[i] = Gauss.compute_weights(nodes, nodes[i - 1], nodes[i])
             smat[n] = Gauss.compute_weights(nodes, nodes[n - 1], end)
         else:
-            raise (ValueError, "Constructing S-Matrix for method '{}' not implemented."
-                               .format(method))
+            raise (ValueError, "Constructing S-Matrix for method '{}' not \
+                                implemented.".format(method))
 
         return smat
 
     @staticmethod
     def legendre_nodes_and_weights(n):
         """
-        computats nodes and weights for the Gauss-Legendre quadrature of order n>1 on [-1, +1]
+        computats nodes and weights for the Gauss-Legendre quadrature of order
+        n>1 on [-1, +1]
         (ported from MATLAB code, reference see below)
 
         (original comment from MatLab source; modified)
         Unlike many publicly available functions, this function is valid for
         n>=46.
-        This is due to the fact that it does not rely on MATLAB's build-in 'root'
-        routines to determine the roots of the Legendre polynomial, but finds the
-        roots by looking for the eigenvalues of an alternative version of the
-        companion matrix of the n'th degree Legendre polynomial.
-        The companion matrix is constructed as a symmetrical matrix, guaranteeing
-        that all the eigenvalues (roots) will be real.
+        This is due to the fact that it does not rely on MATLAB's build-in
+        'root' routines to determine the roots of the Legendre polynomial, but
+        finds the roots by looking for the eigenvalues of an alternative
+        version of the companion matrix of the n'th degree Legendre polynomial.
+        The companion matrix is constructed as a symmetrical matrix,
+        guaranteeing that all the eigenvalues (roots) will be real.
         On the contrary, MATLAB's 'roots' function uses a general form for the
         companion matrix, which becomes unstable at higher orders n, leading to
         complex roots.
 
         (Credit, where credit due)
-        original MATLAB function by: Geert Van Damme <geert@vandamme-iliano.be> (February 21, 2010)
+        original MATLAB function by: Geert Van Damme <geert@vandamme-iliano.be>
+        (February 21, 2010)
         
         :param n: number of integration points
         :type n:  Integer
@@ -222,7 +239,8 @@ class Gauss(Quadrature):
         :raises: ValueError (if `nPoints`<2)
         """
         if n < 2:
-            raise ValueError("Gauss-Legendre quadrature does not work with less than three points.")
+            raise ValueError("Gauss-Legendre quadrature does not work with \
+                              less than three points.")
 
         # Building the companion matrix cm
         # cm is such that det(xI-cm)=P_n(x), with P_n the Legendre polynomial
@@ -242,21 +260,22 @@ class Gauss(Quadrature):
         v = v[:, ind].transpose()
         w = 2.0 * np.asarray(v[:, 0]) ** 2.0
 
-        #print("Gauss.legendre_nodes_and_weights({:d})={: f}".format(n, np.around(x.real, Config.DIGITS)))
         return {'nodes': np.around(x.real, Config.DIGITS),
                 'weights': np.around(w.real, Config.DIGITS)}
 
     @staticmethod
     def lobatto_nodes_and_weights(n_points):
         """
-        Gauss-Lobatto nodes and weights for 3 to 5 integration points (hard coded)
+        Gauss-Lobatto nodes and weights for 3 to 5 integration points
+        (hard coded)
         
         :param n_points: number of integration points
         :type n_points:  Integer
         
         :rtype: Dictionary of Floats with keys `nodes` and `weights`
         
-        :raises: ValueError (if `nPoints`<3), NotImplementedError (if `nPoints`>5)
+        :raises: ValueError (if `nPoints`<3),
+                 NotImplementedError (if `nPoints`>5)
 
         :seealso: http://en.wikipedia.org/wiki/Gaussian_quadrature#Gauss.E2.80.93Lobatto_rules
         """
@@ -288,10 +307,11 @@ class Gauss(Quadrature):
                                 49.0 / 90.0,
                                 1.0 / 10.0]}
         elif n_points < 3:
-            raise ValueError("Gauss-Lobatto quadrature does not work with less than three points.")
+            raise ValueError("Gauss-Lobatto quadrature does not work with \
+                              less than three points.")
         else:
-            raise NotImplementedError("Gauss-Lobatto with {:d} is not implemented yet."
-                                      .format(n_points))
+            raise NotImplementedError("Gauss-Lobatto with {:d} is not \
+                                       implemented yet.".format(n_points))
 
     @staticmethod
     def lobatto_nodes(n_points):
@@ -307,7 +327,8 @@ class Gauss(Quadrature):
         a = (2.0 * j - 1.0) / j
         c = (j - 1.0) / j
 
-        j = np.diag(1 / (a[0:n_points - 1]), 1) + np.diag(c[1:n_points + 1] / a[1:n_points + 1], -1)
+        j = np.diag(1 / (a[0:n_points - 1]), 1) + \
+            np.diag(c[1:n_points + 1] / a[1:n_points + 1], -1)
         # magic trick . . .
         j[n_points - 1, n_points - 2] = 1.0
 
@@ -322,10 +343,12 @@ class Gauss(Quadrature):
         #   3. for x=1 we get the same
         #      that means we have automatically the roots 1 and -1
         #      in this case GaussRadau equiv GaussLobatto
-        #      note 1: this calculations only work for the quadrature weight w(x)=1
+        #      note 1: this calculations only work for the quadrature weight
+        #              w(x)=1
         #      note 2: this ist not the symmetrical form like in GaussLegendre,
         #              hence it is less stable
-        #      note 3: the computed weights are useless (and thus not computed here)
+        #      note 3: the computed weights are useless (and thus not computed
+        #              here)
         [l, v] = linalg.eig(j)
         ind = np.argsort(l)
         x = l[ind]
@@ -351,13 +374,22 @@ class Gauss(Quadrature):
             poly = [1]
             for ar in selection:
                 poly = np.polymul(poly, [1.0 / (nodes[i] - nodes[ar]),
-                                         (1.0 * nodes[ar]) / (nodes[ar] - nodes[i])])
+                                         (1.0 * nodes[ar]) / (nodes[ar] -
+                                                              nodes[i])])
             poly = np.polyint(poly)
             weights[i] = np.polyval(poly, end) - np.polyval(poly, begin)
         return weights
 
     @staticmethod
     def print_iterable(iterable):
+        """
+        prints given Iterable object (e.g. concatenated ranges)
+
+        :param iterable: iterable to print
+        :type iterable:  Iterable
+
+        :rtype: String
+        """
         string = "[ "
         for elem in iterable:
             string += str(elem) + " "
