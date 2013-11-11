e43405d5d60d2e74d85f659a8aaecfeec7da4f47
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-11-11 12:26:51 +0100

    docu: using Sphinx 3.3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pySDC'
-copyright = u'2013, Torbjörn Klatt'
+copyright = u'2013, PinT Research Group, Jülich Supercomputing Centre'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
