1156c5a1086d2a289c2eb42dbb47410ac0006636
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-11-17 20:42:31 +0100

    another commit to fix #17

--- pypint/integrators/node_providers/gauss_lobatto_nodes.py
+++ pypint/integrators/node_providers/gauss_lobatto_nodes.py
@@ -25,7 +25,7 @@ class GaussLobattoNodes(INodes):
         :raises: see :py:func:`.num_nodes`
         """
         self.num_nodes = n_nodes
-        self.__nodes = np.zeros(self.num_nodes)
+        self._nodes = np.zeros(self.num_nodes)
         self._compute_nodes()
 
     @property
@@ -38,20 +38,22 @@ class GaussLobattoNodes(INodes):
         :return: number of nodes
         :rtype:  integer
 
+
         **Setter**
+
         :param n_nodes: number of desired nodes
         :type n_nodes:  integer
 
         :raises: **ValueError** if ``n_nodes`` smaller 2
         """
-        return self.__num_nodes
+        return self._num_nodes
 
     @num_nodes.setter
     def num_nodes(self, n_nodes):
         if n_nodes < 2:
             raise ValueError(self.__qualname__ + ".init(): " +
                              "Gauss-Lobatto with less than 3 nodes doesn't make any sense.")
-        self.__num_nodes = n_nodes
+        self._num_nodes = n_nodes
 
     def _compute_nodes(self):
         # TODO: Implement computation of Gauss-Lobatto nodes
