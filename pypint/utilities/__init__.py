f56822a878e7e489dd545fe770ca23a39f69c794
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-11-18 13:33:29 +0100

    add tracing utility function
    utilities.func_name()
      pretty printing of calling functions name

---
+++ pypint/utilities/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""
+Summary
+-------
+Collection of various utility functions.
+
+.. moduleauthor: 'Torbjörn Klatt' <t.klatt@fz-juelich.de>
+"""
+
+from .tracing import func_name
+
+__all__ = ["func_name"]
