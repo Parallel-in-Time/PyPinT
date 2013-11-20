c5f71bd3f5d64e717b64bfd9ec116b7170d23ad4
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-11-20 09:30:01 +0100

    docu: example for logger

--- pypint/utilities/tracing.py
+++ pypint/utilities/tracing.py
@@ -30,6 +30,13 @@ def func_name(obj=None):
     -------
     formatted function name : str
         Formatted function name of calling function.
+
+    Examples
+    --------
+    >>> from pypint.utilities import func_name
+    >>> def my_func():
+    >>>     print(func_name() + "Hello World!")
+    >>> my_func()
     """
     return "{:s}.{:s}(): "\
            .format(obj.__class__.__name__, inspect.stack()[1][3])
