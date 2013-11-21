292b0ac4c1f20995bd37677adc83c3a27b0a4393
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-11-21 14:24:59 +0100

    tests: extend SdcSolver test

--- tests/pypint/solvers_test/sdc.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-
-import unittest
-from pypint.solvers.sdc import Sdc
-
-
-class SdcTest(unittest.TestCase):
-    def test_initialization(self):
-        _test_obj = Sdc()
---
+++ tests/pypint/solvers_test/sdc_test.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+import unittest
+from unittest.mock import MagicMock
+from unittest.mock import PropertyMock
+from pypint.solvers.sdc import Sdc
+from pypint.problems.i_initial_value_problem import IInitialValueProblem
+
+
+class SdcTest(unittest.TestCase):
+    def setUp(self):
+        self._test_obj = Sdc()
+        self._ivp = MagicMock("Initial Value Problem")
+        self._ivp.__class__ = IInitialValueProblem
+        self._ivp_iv = PropertyMock("Initial Value Property")
+        self._ivp_iv.return_value = 1.0
+        type(self._ivp).initial_value = self._ivp_iv
+        self._ivp_t0 = PropertyMock("Time Start Property")
+        self._ivp_t0.return_value = 0.0
+        type(self._ivp).time_start = self._ivp_t0
+        self._ivp_t1 = PropertyMock("Time End Property")
+        self._ivp_t1.return_value = 1.0
+        type(self._ivp).time_end = self._ivp_t1
+
+    def test_solver_initialization(self):
+        self._test_obj.init(problem=self._ivp)
+
+    def test_constant_one_function(self):
+        self._ivp.evaluate = MagicMock(return_value=1.0)
+        self._test_obj.init(self._ivp)
+        _solution = self._test_obj.run()
