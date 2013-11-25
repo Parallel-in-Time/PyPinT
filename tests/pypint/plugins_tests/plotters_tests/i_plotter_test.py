a99573f869abcbf383a29e8843a66e3c928e1c30
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-11-25 16:16:09 +0100

    tests: fix plotter test names

--- tests/pypint/plugins_tests/plotters_tests/i_plotter_test.py
+++ tests/pypint/plugins_tests/plotters_tests/i_plotter_test.py
@@ -1,15 +1,19 @@
 # coding=utf-8
 
-from pypint.plugins.plotters.i_plotter import IPlotter
 import unittest
+from pypint.plugins.plotters.i_plotter import IPlotter
 from matplotlib import is_interactive
 
 
 class IPlotterTest(unittest.TestCase):
-    def init_with_file_name(self):
+    def test_init_with_file_name(self):
         _pltr = IPlotter(file_name="test_file.svg")
         self.assertFalse(is_interactive())
 
-    def init_without_file_name(self):
+    def test_init_without_file_name(self):
         _pltr = IPlotter()
         self.assertTrue(is_interactive())
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/pypint/plugins_tests/plotters_tests/single_solution_plotter_test.py
+++ tests/pypint/plugins_tests/plotters_tests/single_solution_plotter_test.py
@@ -1,15 +1,19 @@
 # coding=utf-8
 
-from pypint.plugins.plotters.single_solution_plotter import SingleSolutionPlotter
 import unittest
+from pypint.plugins.plotters.single_solution_plotter import SingleSolutionPlotter
 from matplotlib import is_interactive
 
 
 class SingleSolutionPlotterTest(unittest.TestCase):
-    def init_with_file_name(self):
+    def test_init_with_file_name(self):
         _pltr = SingleSolutionPlotter(file_name="test_file.svg")
         self.assertFalse(is_interactive())
 
-    def init_without_file_name(self):
+    def test_init_without_file_name(self):
         _pltr = SingleSolutionPlotter()
         self.assertTrue(is_interactive())
+
+
+if __name__ == "__main__":
+    unittest.main()
