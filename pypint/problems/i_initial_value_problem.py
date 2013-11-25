c3df6d0e802e9fc272e4adb48df65c2ee54bf1bf
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-11-25 09:19:01 +0100

    add docu and function docstrings for problems

--- pypint/problems/i_initial_value_problem.py
+++ pypint/problems/i_initial_value_problem.py
@@ -8,6 +8,20 @@ from pypint.problems.i_problem import IProblem
 
 
 class IInitialValueProblem(IProblem):
+    """
+    Summary
+    -------
+    Basic interface for initial value problems.
+
+    Parameters
+    ----------
+    In addition to the ones of :py:class:`.IProblem` the following options are supported:
+
+    kwargs : dict
+        ``initial_value`` : float | ``numpy.ndarray``
+            Initial value of :math:`u(t_0,\\phi(t_0))` with :math:`t_0` being the time interval
+            start.
+    """
     def __init__(self, *args, **kwargs):
         super(IInitialValueProblem, self).__init__(args, kwargs)
         if "initial_value" in kwargs:
@@ -17,6 +31,21 @@ class IInitialValueProblem(IProblem):
 
     @property
     def initial_value(self):
+        """
+        Summary
+        -------
+        Accessor for the initial value.
+
+        Parameters
+        ----------
+        initial value : float | numpy.ndarray
+            Initial value of the solution.
+
+        Returns
+        -------
+        initial value : float | numpy.ndarray
+            Initial value of the solution.
+        """
         return self._initial_value
 
     @initial_value.setter
@@ -25,5 +54,5 @@ class IInitialValueProblem(IProblem):
 
     def __str__(self):
         str = super(IInitialValueProblem, self).__str__()
-        str += " with initial value {:.3f}".format(self.initial_value)
+        str += r", u({:.2f})={:.2f}".format(self.time_start, self.initial_value)
         return str
