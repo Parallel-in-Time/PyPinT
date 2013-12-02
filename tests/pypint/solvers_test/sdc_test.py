6fae6ea557687d534c382df29be6d7f2f166504b
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-12-02 08:59:45 +0100

    fix tests

--- tests/pypint/solvers_test/sdc_test.py
+++ tests/pypint/solvers_test/sdc_test.py
@@ -39,7 +39,7 @@ class SdcTest(NumpyAwareTestCase):
 
     def test_lambda_u(self):
         _lambda_u = LambdaU(lmbda=-1.0)
-        self._test_obj.init(problem=_lambda_u, num_nodes=9)
+        self._test_obj.init(problem=_lambda_u, num_time_steps=2, num_nodes=9)
         _solution = self._test_obj.run()
         self.assertEqual(_solution.used_iterations, 6,
                          "Explicit SDC should converge in 6 iterations.")
