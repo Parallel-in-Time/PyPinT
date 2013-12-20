e280f97341188ac38727b53aa26415c5aea95bf7
Torbjörn Klatt <t.klatt@fz-juelich.de>
2013-12-20 13:35:55 +0100

    providing exact solution functions for problems via a mixin

--- pypint/problems/__init__.py
+++ pypint/problems/__init__.py
@@ -5,3 +5,9 @@ Problems for Iterative Time Solvers
 .. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
 .. moduleauthor:: Dieter Moser <d.moser@fz-juelich.de>
 """
+
+from .i_problem import IProblem
+from .i_initial_value_problem import IInitialValueProblem
+from .has_exact_mixin import HasExactSolutionMixin, problem_has_exact_solution
+
+__all__ = ['IProblem', 'IInitialValueProblem', 'HasExactSolutionMixin', 'problem_has_exact_solution']
