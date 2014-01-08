a42b4e24f23fa1a260f1e139797f3244f9f37df7
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-01-08 10:06:27 +0100

    reorganizing scripts; add runs dir
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- run_dbg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$1
-PYPINT=${2:-`pwd`}
-
-PYTHONPATH=$PYPINT python3 $SCRIPT | sed -n 's/^.*: [!]*> //p'
-
---
+++ scripts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT=$1
+shift
+
+PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: > //p'
+
