f1811f930e2f0181a83abec112e4861194d2bb90
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-02-03 11:44:27 +0100

    diagnosis values (Error, Residual) & norm wrappers
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

---
+++ pypint/solvers/diagnosis/norms.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+"""
+.. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
+"""
+
+import numpy as np
+
+from pypint.solvers.diagnosis.i_diagnosis_value import IDiagnosisValue
+from pypint.utilities import assert_is_instance
+
+
+def supremum_norm(vec):
+    """
+    Summary
+    -------
+    Computes uniform (or infinity) norm of given vector or :py:class:`.IDiagnosisValue`.
+
+    Extended Summary
+    ----------------
+    Uses numpy's norm function internally.
+
+    Parameters
+    ----------
+    vec : :py:class:`numpy.ndarray` or :py:class:`.IDiagnosisValue`
+
+    Returns
+    -------
+    sup-norm : :py:class:`numpy.ndarray`
+    """
+    assert_is_instance(vec, (np.ndarray, IDiagnosisValue),
+                       "The infinity requires a numpy.ndarray or IDiagnosisValue: NOT {}"
+                       .format(vec.__class__.__name__))
+    return \
+        np.linalg.norm(vec, np.inf) if isinstance(vec, np.ndarray) else np.linalg.norm(vec.value, np.inf)
+
+
+def two_norm(vec):
+    """
+    Summary
+    -------
+    Computes two-norm of given vector or :py:class:`.IDiagnosisValue`.
+
+    Extended Summary
+    ----------------
+    Uses numpy's norm function internally.
+
+    Parameters
+    ----------
+    vec : :py:class:`numpy.ndarray` or :py:class:`.IDiagnosisValue`
+
+    Returns
+    -------
+    two-norm : :py:class:`numpy.ndarray`
+    """
+    assert_is_instance(vec, (np.ndarray, IDiagnosisValue),
+                       "The infinity requires a numpy.ndarray or IDiagnosisValue: NOT {}"
+                       .format(vec.__class__.__name__))
+    return \
+        np.linalg.norm(vec) if isinstance(vec, np.ndarray) else np.linalg.norm(vec.value)
+
+
+__all__ = ['supremum_norm', 'two_norm']
