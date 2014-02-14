078f84cc67445a2010d4c46be9714cee51bfe88c
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-02-14 16:41:55 +0100

    sdc: fix state reference
    refs #33 - move Solver's state into object hierarchy to pass around
    refs #31 - Refactor SDC Solver
    
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- pypint/solvers/cores/sdc_solver_core.py
+++ pypint/solvers/cores/sdc_solver_core.py
@@ -6,6 +6,7 @@ from pypint.solvers.cores.i_solver_core import ISolverCore
 from pypint.problems.has_exact_solution_mixin import problem_has_exact_solution
 from pypint.solvers.diagnosis import Error, Residual
 from pypint.utilities import assert_is_key
+from pypint import LOG
 
 
 class SdcSolverCore(ISolverCore):
@@ -39,12 +40,13 @@ class SdcSolverCore(ISolverCore):
     def compute_residual(self, state, **kwargs):
         # LOG.debug("computing residual")
         super(SdcSolverCore, self).compute_residual(state, **kwargs)
+        LOG.debug("Residual: {} + {} * {} - {}".format(state.current_time_step.initial.solution.value,state.current_time_step.delta_time_step,state.current_step.integral,state.current_step.solution.value))
         state.current_step.solution.residual = Residual(
             abs(state.current_time_step.initial.solution.value
-                + state.delta_interval * state.current_step.integral
+                + state.current_time_step.delta_time_step * state.current_step.integral
                 - state.current_step.solution.value)
         )
-        # LOG.debug("Residual: {}".format(state.current_step.solution.residual.value))
+        LOG.debug("Residual: {}".format(state.current_step.solution.residual.value))
 
     def compute_error(self, state, **kwargs):
         super(SdcSolverCore, self).compute_error(state, **kwargs)
@@ -55,6 +57,9 @@ class SdcSolverCore(ISolverCore):
         _problem = kwargs['problem']
 
         if problem_has_exact_solution(_problem, self):
+            # LOG.debug("Error for t={:.3f}: {} - {}".format(state.current_step.time_point,
+            #                                               state.current_step.solution.value,
+            #                                               _problem.exact(state.current_step.time_point)))
             state.current_step.solution.error = Error(
                 abs(state.current_step.solution.value - _problem.exact(state.current_step.time_point))
             )
@@ -64,13 +69,16 @@ class SdcSolverCore(ISolverCore):
             pass
 
     def _previous_iteration_previous_step(self, state):
-        if state.previous_iteration_index and state.previous_step_index:
-            return state.previous_iteration[state.current_time_step_index][state.previous_step_index]
+        if state.previous_iteration_index is not None:
+            if state.previous_step_index is not None:
+                return state.previous_iteration[state.current_time_step_index][state.previous_step_index]
+            else:
+                return state.previous_iteration[state.current_time_step_index].initial
         else:
             return state.initial
 
     def _previous_iteration_current_step(self, state):
-        if state.previous_iteration_index:
+        if state.previous_iteration_index is not None:
             return state.previous_iteration[state.current_time_step_index][state.current_step_index]
         else:
             return state.initial
