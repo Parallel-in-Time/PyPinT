e045010cd6900eb2964e59be4b5d4d7cd12497ed
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-02-21 14:03:55 +0100

    tests: fix and improve SDC tests
    refs #37 - Update/Write/Extend unit tests
    
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- tests/pypint/solvers_test/sdc_test.py
+++ tests/pypint/solvers_test/sdc_test.py
@@ -26,8 +26,9 @@ def _run_sdc_with_problem(problem, core, num_time_steps, num_nodes, max_iter):
         # assert_numpy_array_almost_equal(_solution.solution(-1)[_node_index].value,
         #                                 problem.exact(_solution.solution(-1)[_node_index].time_point),
         #                                 places=2)
-    assert_in(thresh.has_reached(), ['error', 'residual'], "Termination criteria should be 'error' or 'residual'.")
-    assert_is_not(thresh.has_reached(), "iterations", "Maximum Number of iterations should not be reached.")
+    assert_true(len(frozenset(thresh.has_reached()).intersection(frozenset(('error', 'residual')))) > 0,
+                "Termination criteria should be 'error' or 'residual'.")
+    assert_not_in('iterations', thresh.has_reached(), "Maximum Number of iterations should not be reached.")
     assert_numpy_array_almost_equal(_solution.solution(-1)[-1].value,
                                     problem.exact(_solution.solution(-1)[-1].time_point),
                                     places=6)
@@ -39,7 +40,7 @@ def _constant_minus_one_function(sdc_core, num_time_steps, num_nodes, max_iter):
 
 
 def _lambda_u_function(sdc_core, num_time_steps, num_nodes, max_iter):
-    problem = LambdaU(lmbda=complex(-1.0, -1.0))
+    problem = LambdaU(lmbda=complex(-1.0, 1.0))
     _run_sdc_with_problem(problem, sdc_core, num_time_steps, num_nodes, max_iter)
 
 
@@ -92,14 +93,14 @@ def test_lambda_u_with_explicit_sdc():
 
 def test_lambda_u_with_implicit_sdc():
     _expected_iterations = {
-        1: {5: MAX_ITER, 7: 10},
-        2: {5: MAX_ITER, 7: 9},
-        3: {5: 9, 7: 9}
+        1: {5: MAX_ITER, 7: 11},
+        2: {5: 10, 7: 10},
+        3: {5: 10, 7: 10}
     }
     for _num_time_steps in [1, 2, 3]:
         for _num_nodes in [5, 7]:
-            if _num_nodes == 5 and _num_time_steps in [1, 2]:
-                # those do not converge
+            if _num_nodes == 5 and _num_time_steps == 1:
+                # this will not converge below error of 3.28e-7
                 pass
             else:
                 yield _lambda_u_function, ImplicitSdcCore, \
@@ -108,14 +109,14 @@ def test_lambda_u_with_implicit_sdc():
 
 def test_lambda_u_with_semi_implicit_sdc():
     _expected_iterations = {
-        1: {5: MAX_ITER, 7: 10},
-        2: {5: 10, 7: 9},
-        3: {5: 9, 7: 9}
+        1: {5: MAX_ITER, 7: 9},
+        2: {5: 9, 7: 8},
+        3: {5: 8, 7: 8}
     }
     for _num_time_steps in [1, 2, 3]:
         for _num_nodes in [5, 7]:
             if _num_time_steps == 1 and _num_nodes == 5:
-                # this will not converge below error of 1e-6
+                # this will not converge below error of 3.28e-7
                 pass
             else:
                 yield _lambda_u_function, SemiImplicitSdcCore, \
