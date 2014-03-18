fba1b98039eb73ddf849d3cdbceb927a95f7ec5b
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-03-18 09:31:04 +0100

    add+impl simple communicators
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

---
+++ pypint/communicators/forward_sending_messaging.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+"""
+.. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
+"""
+from pypint.communicators.i_communication_provider import ICommunicationProvider
+from pypint.utilities import assert_condition, assert_is_key, assert_is_instance
+
+
+class ForwardSendingMessaging(ICommunicationProvider):
+    """A linear forward-directed communication pattern
+    """
+    def __init__(self, *args, **kwargs):
+        super(ForwardSendingMessaging, self).__init__(*args, **kwargs)
+        self._previous = None
+        self._next = None
+
+    def send(self, *args, **kwargs):
+        """Sends given message to the next communicator
+
+        See Also
+        --------
+        :py:meth:`.ICommunicationProvider.write_buffer`
+            for allowed arguments
+        """
+        super(ForwardSendingMessaging, self).send(*args, **kwargs)
+        self._next.write_buffer(*args, **kwargs)
+
+    def receive(self, *args, **kwargs):
+        """Returns this communicator's buffer
+
+        Returns
+        -------
+        message : :py:class:`.Message`
+        """
+        super(ForwardSendingMessaging, self).receive(*args, **kwargs)
+        return self.buffer
+
+    def link_solvers(self, *args, **kwargs):
+        """Links the given communicators with this communicator
+
+        Parameters
+        ----------
+        previous : :py:class:`.ForwardSendingMessaging`
+            communicator of the previous solver
+        next : :py:class:`.ForwardSendingMessaging`
+            communicator of the next solver
+
+        Raises
+        ------
+        ValueError
+            if one of the two communicators of the specified type is not given
+        """
+        super(ForwardSendingMessaging, self).link_solvers(*args, **kwargs)
+        assert_condition(len(kwargs) == 2,
+                         ValueError, "Exactly two solver processes must be given: NOT %d" % len(kwargs),
+                         self)
+
+        assert_is_key(kwargs, 'previous', "Previous solver must be given.", self)
+        assert_is_instance(kwargs['previous'], ForwardSendingMessaging,
+                           "Previous Communicator must also be a ForwardSendingMessaging instance: NOT %s"
+                           % kwargs['previous'].__class__.__name__,
+                           self)
+        self._previous = kwargs['previous']
+
+        assert_is_key(kwargs, 'next', "Next solver must be given.", self)
+        assert_is_instance(kwargs['next'], ForwardSendingMessaging,
+                           "Next Communicator must also be a ForwardSendingMessaging instance: NOT %s"
+                           % kwargs['next'].__class__.__name__,
+                           self)
+        self._next = kwargs['next']
--- pypint/communicators/i_communication_provider.py
+++ pypint/communicators/i_communication_provider.py
@@ -1,29 +1,111 @@
 # coding=utf-8
 """
-
 .. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
 """
+from copy import deepcopy
+
+from pypint.communicators import Message
+from pypint.utilities import assert_is_instance, assert_condition
 
 
 class ICommunicationProvider(object):
     """Interface for communication providers
+
+    Notes
+    -----
+    This communication interface uses buffered communication.
     """
-    def __init__(self):
-        self._num_compute_nodes = None
-        self._compute_nodes = None
+    def __init__(self, *args, **kwargs):
+        """
+        Parameters
+        ----------
+        buffer : :py:class:`.Message`
+            initial buffer
 
-    def get_neighbours(self, node_id):
-        """Accessor for the neighbours of given node
+        Raises
+        ------
+        ValueError
+            if ``buffer`` is not a :py:class:`.Message`
+        """
+        self._buffer = None
+
+        if 'buffer' in kwargs:
+            assert_is_instance(kwargs['buffer'], Message,
+                               "Initial buffer must be a Message: NOT %s" % kwargs['buffer'].__class__.__name__,
+                               self)
+            self._buffer = kwargs['buffer']
+        else:
+            self._buffer = Message()
+
+    def send(self, *args, **kwargs):
+        """Sending data to a specified communicator
+
+        Notes
+        -----
+        Behaviour is implementation dependent.
         """
         pass
 
-    def get_node(self, coordinate):
-        """Accessor for a node of given coordinate
+    def receive(self, *args, **kwargs):
+        """Receiving data from a specified communicator
+
+        Notes
+        -----
+        Behaviour is implementation dependent.
         """
         pass
+
+    def link_solvers(self, *args, **kwargs):
+        """Linking the specified communicators with this one
+
+        Notes
+        -----
+        Behaviour is implementation dependent.
+        """
+        pass
+
+    def write_buffer(self, *args, **kwargs):
+        """Writes data into this communicator's buffer
+
+        Parameters
+        ----------
+        value :
+            data values to be send to the next solver
+        time_point : :py:class:`float`
+            time point of the data values
+        flag : :py:class:`.Message.SolverFlag`
+            message flag
+
+        Raises
+        ------
+        ValueError
+
+            * if no arguments are given
+            * if ``time_point`` is not a :py:class:`float`
+        """
+        assert_condition(len(args) > 0 or len(kwargs) > 0,
+                         ValueError, "At least one argument must be given: NOT %d (args) or %d (kwargs)"
+                                     % (len(args), len(kwargs)),
+                         self)
+
+        if 'value' in kwargs:
+            self._buffer.value = deepcopy(kwargs['value'])
+
+        if 'time_point' in kwargs:
+            assert_is_instance(kwargs['time_point'], float,
+                               "Time point must be a float: NOT %s" % kwargs['time_point'].__class__.__name__,
+                               self)
+            self._buffer.time_point = deepcopy(kwargs['time_point'])
+
+        if 'flag' in kwargs:
+            self._buffer.flag = deepcopy(kwargs['flag'])
 
     @property
-    def num_compute_nodes(self):
-        """Read-only accessor for the total number of nodes
+    def buffer(self):
+        """Read-only accessor for this communicator's buffer
+
+        Returns
+        -------
+        buffer : :py:class:`.Message`
         """
-        return self._num_compute_nodes
+        return self._buffer
