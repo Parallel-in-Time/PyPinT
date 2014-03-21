c749c06aa2b8d1d641cc3f35414d654dadb678a0
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-03-21 14:30:18 +0100

    tests: add, extend and improve SDC solver tests
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- tests/pypint/solvers_test/sdc_test.py
+++ tests/pypint/solvers_test/sdc_test.py
@@ -13,10 +13,11 @@ from examples.problems.constant import Constant
 
 
 MAX_ITER = 100
+PRECISION = 6
 
 
-def _run_sdc_with_problem(problem, core, num_time_steps, num_nodes, max_iter):
-    thresh = ThresholdCheck(max_threshold=max_iter + 1, conditions=("error", "residual", "iterations"),
+def _run_sdc_with_problem(problem, core, num_time_steps, num_nodes, max_iter, precision):
+    thresh = ThresholdCheck(max_threshold=max_iter + 1, conditions=('error', 'residual', 'solution reduction', 'error reduction', 'iterations'),
                             min_threshold=1e-7)
     _sdc = Sdc()
     _sdc.init(threshold=thresh, problem=problem, num_time_steps=num_time_steps, num_nodes=num_nodes)
@@ -26,101 +27,96 @@ def _run_sdc_with_problem(problem, core, num_time_steps, num_nodes, max_iter):
         # assert_numpy_array_almost_equal(_solution.solution(-1)[_node_index].value,
         #                                 problem.exact(_solution.solution(-1)[_node_index].time_point),
         #                                 places=2)
-    assert_true(len(frozenset(thresh.has_reached()).intersection(frozenset(('error', 'residual')))) > 0,
+    assert_true(len(frozenset(thresh.has_reached()).intersection(frozenset(('error', 'solution reduction', 'error reduction', 'residual')))) > 0,
                 "Termination criteria should be 'error' or 'residual'.")
     assert_not_in('iterations', thresh.has_reached(), "Maximum Number of iterations should not be reached.")
     assert_numpy_array_almost_equal(_solution.solution(-1)[-1].value,
                                     problem.exact(_solution.solution(-1)[-1].time_point),
-                                    places=6)
+                                    places=precision)
 
 
-def _constant_minus_one_function(sdc_core, num_time_steps, num_nodes, max_iter):
+def _constant_minus_one_function(sdc_core, num_time_steps, num_nodes, iter_precision):
     problem = Constant(constant=-1.0, shift=1.0)
-    _run_sdc_with_problem(problem, sdc_core, num_time_steps, num_nodes, max_iter)
+    precision = iter_precision['prec'] if 'prec' in iter_precision else PRECISION
+    _run_sdc_with_problem(problem, sdc_core, num_time_steps, num_nodes, iter_precision['iter'], precision)
 
 
-def _lambda_u_function(sdc_core, num_time_steps, num_nodes, max_iter):
+def _lambda_u_function(sdc_core, num_time_steps, num_nodes, iter_precision):
     problem = LambdaU(lmbda=complex(-1.0, 1.0))
-    _run_sdc_with_problem(problem, sdc_core, num_time_steps, num_nodes, max_iter)
+    precision = iter_precision['prec'] if 'prec' in iter_precision else PRECISION
+    _run_sdc_with_problem(problem, sdc_core, num_time_steps, num_nodes, iter_precision['iter'], precision)
 
 
 def test_constant_minus_one_function_with_explicit_sdc():
     _expected_iterations = {
-        1: {3: 2, 5: 2, 7: 2},
-        2: {3: 2, 5: 2, 7: 2},
-        3: {3: 2, 5: 2, 7: 2}
+        1: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}},
+        2: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}},
+        3: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}}
     }
-    for _num_time_steps in [1, 2, 3]:
-        for _num_nodes in [3, 5, 7]:
+    for _num_time_steps in _expected_iterations.keys():
+        for _num_nodes in _expected_iterations[_num_time_steps].keys():
             yield _constant_minus_one_function, ExplicitSdcCore, \
                 _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
 
 
 def test_constant_minus_one_function_with_implicit_sdc():
     _expected_iterations = {
-        1: {3: 2, 5: 2, 7: 2},
-        2: {3: 2, 5: 2, 7: 2},
-        3: {3: 2, 5: 2, 7: 2}
+        1: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}},
+        2: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}},
+        3: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}}
     }
-    for _num_time_steps in [1, 2, 3]:
-        for _num_nodes in [3, 5, 7]:
+    for _num_time_steps in _expected_iterations.keys():
+        for _num_nodes in _expected_iterations[_num_time_steps].keys():
             yield _constant_minus_one_function, ImplicitSdcCore, \
                 _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
 
 
 def test_constant_minus_one_function_with_semi_implicit_sdc():
     _expected_iterations = {
-        1: {3: 2, 5: 2, 7: 2},
-        2: {3: 2, 5: 2, 7: 2},
-        3: {3: 2, 5: 2, 7: 2}
+        1: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}},
+        2: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}},
+        3: {3: {'iter': 1}, 5: {'iter': 1}, 7: {'iter': 1}}
     }
-    for _num_time_steps in [1, 2, 3]:
-        for _num_nodes in [3, 5, 7]:
+    for _num_time_steps in _expected_iterations.keys():
+        for _num_nodes in _expected_iterations[_num_time_steps].keys():
             yield _constant_minus_one_function, SemiImplicitSdcCore, \
                 _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
 
 
 def test_lambda_u_with_explicit_sdc():
     _expected_iterations = {
-        2: {5: 8, 7: 7},
-        3: {5: 7, 7: 7}
+        1: {3: {'iter': 22, 'prec': 2}, 5: {'iter': 12}, 7: {'iter': 9}},
+        2: {3: {'iter': 12, 'prec': 3}, 5: {'iter': 8}, 7: {'iter': 7}},
+        3: {3: {'iter': 10, 'prec': 4}, 5: {'iter': 7}, 7: {'iter': 6}}
     }
-    for _num_time_steps in [2, 3]:
-        for _num_nodes in [5, 7]:
+    for _num_time_steps in _expected_iterations.keys():
+        for _num_nodes in _expected_iterations[_num_time_steps].keys():
             yield _lambda_u_function, ExplicitSdcCore, \
                 _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
 
 
 def test_lambda_u_with_implicit_sdc():
     _expected_iterations = {
-        1: {5: MAX_ITER, 7: 11},
-        2: {5: 10, 7: 10},
-        3: {5: 10, 7: 10}
+        1: {3: {'iter': 12, 'prec': 2}, 5: {'iter': 10}, 7: {'iter': 8}},
+        2: {3: {'iter': 10, 'prec': 3}, 5: {'iter': 7}, 7: {'iter': 7}},
+        3: {3: {'iter': 9, 'prec': 4}, 5: {'iter': 7}, 7: {'iter': 6}}
     }
-    for _num_time_steps in [1, 2, 3]:
-        for _num_nodes in [5, 7]:
-            if _num_nodes == 5 and _num_time_steps == 1:
-                # this will not converge below error of 3.28e-7
-                pass
-            else:
-                yield _lambda_u_function, ImplicitSdcCore, \
-                    _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
+    for _num_time_steps in _expected_iterations.keys():
+        for _num_nodes in _expected_iterations[_num_time_steps].keys():
+            yield _lambda_u_function, ImplicitSdcCore, \
+                _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
 
 
 def test_lambda_u_with_semi_implicit_sdc():
     _expected_iterations = {
-        1: {5: MAX_ITER, 7: 9},
-        2: {5: 9, 7: 8},
-        3: {5: 8, 7: 8}
+        1: {3: {'iter': 14, 'prec': 2}, 5: {'iter': 11}, 7: {'iter': 9}},
+        2: {3: {'iter': 10, 'prec': 3}, 5: {'iter': 8}, 7: {'iter': 7}},
+        3: {3: {'iter': 9, 'prec': 4}, 5: {'iter': 7}, 7: {'iter': 6}}
     }
-    for _num_time_steps in [1, 2, 3]:
-        for _num_nodes in [5, 7]:
-            if _num_time_steps == 1 and _num_nodes == 5:
-                # this will not converge below error of 3.28e-7
-                pass
-            else:
-                yield _lambda_u_function, SemiImplicitSdcCore, \
-                    _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
+    for _num_time_steps in _expected_iterations.keys():
+        for _num_nodes in _expected_iterations[_num_time_steps].keys():
+            yield _lambda_u_function, SemiImplicitSdcCore, \
+                _num_time_steps, _num_nodes, _expected_iterations[_num_time_steps][_num_nodes]
 
 
 class SdcTest(NumpyAwareTestCase):
