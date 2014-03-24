4017494d4814c142df03ad07fdc69abc6970d4b7
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-03-24 15:36:22 +0100

    new advanced logging capabilities
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- pypint/utilities/logging.py
+++ pypint/utilities/logging.py
@@ -5,13 +5,44 @@
 """
 import logging as log
 from sys import stdout
+from collections import OrderedDict
 
 loggers = []
+
+VERBOSITY_LVL1 = '!> '
+VERBOSITY_LVL2 = '>> '
+VERBOSITY_LVL3 = ' > '
+
+SEPARATOR_LVL1 = '#' * 80
+SEPARATOR_LVL2 = '-' * 80
+SEPARATOR_LVL3 = '.' * 80
+
+
+def print_logging_message_tree(messages):
+    for _key1, _value1 in messages.items():
+        if isinstance(_value1, (dict, OrderedDict)):
+            if _key1 != '':
+                logger().info("%s%s:" % (VERBOSITY_LVL1, _key1))
+            for _key2, _value2 in _value1.items():
+                if isinstance(_value2, (dict, OrderedDict)):
+                    if _key2 != '':
+                        logger().info("%s  %s:" % (VERBOSITY_LVL1, _key2))
+                    for _key3, _value3 in _value2.items():
+                        if isinstance(_value3, (dict, OrderedDict)):
+                            if _key3 != '':
+                                logger().info("%s    %s:" % (VERBOSITY_LVL1, _key3))
+                            for _key4, _value4 in _value3.items():
+                                logger().info("{}      {: <28s} {}".format(VERBOSITY_LVL1, "%s: " % _key4, _value4))
+                        else:
+                            logger().info("{}    {: <30s} {}".format(VERBOSITY_LVL1, "%s: " % _key3, _value3))
+                else:
+                    logger().info("{}  {: <32s} {}".format(VERBOSITY_LVL1, "%s: " % _key2, _value2))
+        else:
+            logger().info("{}{: <34s} {}".format(VERBOSITY_LVL1, "%s: " % _key1, _value1))
 
 
 def logger():
     """Accessor for a standard logger
-
     """
     global loggers
 
@@ -28,3 +59,10 @@ def logger():
         _logger.addHandler(_handler)
         loggers.append(_logger)
         return logger()
+
+
+__all__ = [
+    'VERBOSITY_LVL1', 'VERBOSITY_LVL2', 'VERBOSITY_LVL3',
+    'SEPARATOR_LVL1', 'SEPARATOR_LVL2', 'SEPARATOR_LVL3',
+    'print_logging_message_tree'
+]
