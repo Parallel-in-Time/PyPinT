30bac93c5907191ff0a2f31103c67e44da830653
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-03-24 15:39:00 +0100

    scripts: adjusted run script to new logging behaviour
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+VERBOSITY=$1
+shift
 SCRIPT=$1
 shift
 
-PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: > //p'
+if [[ $VERBOSITY -eq 1 ]]; then
+  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: !> //p'
+elif [[ $VERBOSITY -eq 2 ]]; then
+  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: [!>]> //p'
+else
+  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: [!> ]> //p'
+fi
 
