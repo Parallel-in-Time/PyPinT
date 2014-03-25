94f4918e238938f9bb26c5aac36712fdae21f810
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-03-25 11:37:34 +0100

    tests: fix test cases for altered tracing output
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- tests/pypint/utilities_tests/tracing_test.py
+++ tests/pypint/utilities_tests/tracing_test.py
@@ -6,8 +6,8 @@ from pypint.utilities.tracing import func_name
 
 class TracingTest(unittest.TestCase):
     def test_func_name(self):
-        self.assertRegex(func_name(self), "TracingTest\.test_func_name")
-        self.assertRegex(func_name(), "unknown\.test_func_name")
+        self.assertRegex(func_name(self), "^TracingTest<0x[0-9a-f]*>\.test_func_name")
+        self.assertRegex(func_name(), "^test_func_name")
 
 
 if __name__ == "__main__":
