7df2d017a96ed538fbdd81d91b49fe7d019fe8c9
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-04-01 10:55:57 +0200

    adjust run shell script to different logging output format

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,10 +6,10 @@ SCRIPT=$1
 shift
 
 if [[ $VERBOSITY -eq 1 ]]; then
-  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: !> //p'
+  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^\[.*\] !> //p'
 elif [[ $VERBOSITY -eq 2 ]]; then
-  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: [!>]> //p'
+  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^\[.*\] [!>]> //p'
 else
-  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^.*: [!> ]> //p'
+  PYTHONPATH=$PYPINT python3 $SCRIPT "$@" | sed -n 's/^\[.*\] [!> ]> //p'
 fi
 
