c899d56ec404c53e3c5913c2cb7e27a525f4f505
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-04-03 10:38:21 +0200

    fix setup.py - build, install, sdist, bdist are now working
    TODO: bdist_rpm produces invalid rpms according to rpmlint

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
+from sys import version_info
 
 ##
 # this block is taken from
@@ -19,29 +20,55 @@ else:
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.abspath(os.path.dirname(__file__)), fname)).read()
 
 
-setup(
-    name="PyPinT",
-    version=verstr,
-    author="Torbjörn Klatt, Dieter Moser",
-    author_email="t.klatt@fz-juelich.de, d.moser@fz-juelich.de",
-    description="A Python framework for Parallel-in-Time integration routines.",
-    long_description=read('README.md'),
-    license="MIT",
-    url="http://fz-juelich.de/ias/jsc/pint",
-    install_requires=['numpy', 'scipy', 'matplotlib'],
-    packages=['pypint', 'tests'],
-    test_suite="tests",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.2",
-        "Topic :: Scientific/Engineering :: Mathematics"
-    ]
-)
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Topic :: Scientific/Engineering :: Mathematics"
+]
+
+DEPENDENCIES = [
+    "numpy>=1.6.1",
+    "scipy>=0.9.0",
+    "matplotlib>=1.2.0",
+    "logbook>=0.6.0",
+    "configobj>=5.0.2"
+]
+
+if version_info.major == 3 and version_info.minor < 4:
+    DEPENDENCIES.append("enum34")
+
+TEST_DEPENDENCIES = [
+    "nose>=1.3.1"
+]
+
+metadata = {
+    'name': "PyPinT",
+    'version': verstr,
+    'packages': find_packages(),
+
+    'install_requires': DEPENDENCIES,
+    'include_package_data': True,
+
+    'test_suite': "tests",
+    'tests_require': TEST_DEPENDENCIES,
+
+    'author': "Torbjörn Klatt, Dieter Moser",
+    'author_email': "t.klatt@fz-juelich.de, d.moser@fz-juelich.de",
+    'description': "a Python framework for Parallel-in-Time integration routines",
+    'long_description': read('README.md'),
+    'license': "MIT",
+    'url': "https://github.com/torbjoernk/PyPinT",
+
+    'classifiers': CLASSIFIERS
+}
+
+setup(**metadata)
