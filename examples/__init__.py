14da35148092062e6e9f4d73ec7bfbb282083cb0
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-04-03 13:56:30 +0200

    examples: made them a module

---
+++ examples/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+"""Examples demonstrating usage of PyPinT
+
+.. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
+"""
--- examples/problems/__init__.py
+++ examples/problems/__init__.py
@@ -3,3 +3,8 @@
 
 .. moduleauthor:: Torbjörn Klatt <t.klatt@fz-juelich.de>
 """
+
+from examples.problems.constant import Constant
+from examples.problems.lambda_u import LambdaU
+
+__all__ = ['Constant', 'LambdaU']
