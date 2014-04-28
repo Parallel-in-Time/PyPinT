951a6bc5752a177eac6bdfa905c4fd35c1d97734
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-04-28 17:06:15 +0200

    solvers: cores: Explicit MLSDC fixes

--- pypint/solvers/cores/explicit_mlsdc_core.py
+++ pypint/solvers/cores/explicit_mlsdc_core.py
@@ -2,10 +2,13 @@
 """
 .. moduleauthor:: Torbjörn Klatt <t.kaltt@fz-juelich.de>
 """
+import numpy as np
+
 from pypint.solvers.cores.mlsdc_solver_core import MlSdcSolverCore
 from pypint.solvers.states.mlsdc_solver_state import MlSdcSolverState
 from pypint.problems import IProblem
 from pypint.utilities import assert_is_instance, assert_named_argument
+from pypint.utilities.logging import LOG
 
 
 class ExplicitMlSdcCore(MlSdcSolverCore):
@@ -37,7 +40,12 @@ class ExplicitMlSdcCore(MlSdcSolverCore):
         _problem = kwargs['problem']
 
         _previous_step = state.previous_step
-        _previous_iteration_previous_step = self._previous_iteration_previous_step(state)
+
+        if not state.current_iteration.on_finest_level:
+            # LOG.debug("   taking previous step of this iteration instead")
+            _previous_iteration_previous_step = state.current_iteration.current_level.previous_step
+        else:
+            _previous_iteration_previous_step = self._previous_iteration_previous_step(state)
 
         if not _previous_step.rhs_evaluated:
             _previous_step.rhs = _problem.evaluate(state.current_step.time_point, _previous_step.value)
@@ -45,16 +53,10 @@ class ExplicitMlSdcCore(MlSdcSolverCore):
             _previous_iteration_previous_step.rhs = \
                 _problem.evaluate(state.current_step.time_point, _previous_iteration_previous_step.value)
 
-        if _previous_step.fas_correction:
-            _previous_rhs = _previous_step.rhs + _previous_step.fas_correction
-        else:
-            _previous_rhs = _previous_step.rhs
-
-        if _previous_iteration_previous_step.fas_correction:
-            _previous_iteration_previous_rhs = \
-                _previous_iteration_previous_step.rhs + _previous_iteration_previous_step.fas_correction
-        else:
-            _previous_iteration_previous_rhs = _previous_iteration_previous_step.rhs
+        _fas = np.zeros(_previous_step.rhs.shape, dtype=_previous_step.rhs.dtype)
+        if _previous_step.has_fas_correction():
+            # LOG.debug("   previous step has FAS")
+            _fas = _previous_step.fas_correction
 
         # Note: \Delta_t is always 1.0 as it's part of the integral
         # using step-wise formula
@@ -62,11 +64,11 @@ class ExplicitMlSdcCore(MlSdcSolverCore):
         #   u_{m+1}^{k+1} = u_m^{k+1} + \Delta_\tau [ F(u_m^{k+1}) - F(u_m^k) ] + \Delta_t I_m^{m+1}(F(u^k))
         state.current_step.value = \
             (_previous_step.value
-             + state.current_step.delta_tau * (_previous_rhs - _previous_iteration_previous_rhs)
-             + state.current_step.integral)
-        # print("%s = %s + %s * (%s - %s) + %s" %
-        #       (state.current_step.value, _previous_step.value, state.current_step.delta_tau, _previous_rhs,
-        #        _previous_iteration_previous_rhs, state.current_step.integral))
+             + state.current_step.delta_tau * (_previous_step.rhs - _previous_iteration_previous_step.rhs)
+             + state.current_step.integral + _fas)
+        # LOG.debug("%s = %s + %s * (%s - %s) + %s + %s" %
+        #           (state.current_step.value, _previous_step.value, state.current_step.delta_tau, _previous_step.rhs,
+        #            _previous_iteration_previous_step.rhs, state.current_step.integral, _fas))
 
 
 __all__ = ['ExplicitMlSdcCore']
