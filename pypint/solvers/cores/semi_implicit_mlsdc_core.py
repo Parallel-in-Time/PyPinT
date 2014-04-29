bb4700ef3fade5fde592162027220827b07c4867
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-04-29 10:29:44 +0200

    solvers: cores: SemiImplicitMLSDC works

--- pypint/solvers/cores/semi_implicit_mlsdc_core.py
+++ pypint/solvers/cores/semi_implicit_mlsdc_core.py
@@ -2,6 +2,8 @@
 """
 .. moduleauthor:: Torbjörn Klatt <t.kaltt@fz-juelich.de>
 """
+import numpy as np
+
 from pypint.solvers.cores.mlsdc_solver_core import MlSdcSolverCore
 from pypint.solvers.states.mlsdc_solver_state import MlSdcSolverState
 from pypint.problems import IProblem
@@ -42,16 +44,39 @@ class SemiImplicitMlSdcCore(MlSdcSolverCore):
         assert_named_argument('problem', kwargs, types=IProblem, descriptor="Problem", checking_obj=self)
         _problem = kwargs['problem']
 
-        _previous_iteration_current_step = self._previous_iteration_current_step(state)
-        _previous_iteration_previous_step = self._previous_iteration_previous_step(state)
+        if not state.current_iteration.on_finest_level:
+            _previous_iteration_current_step = state.current_iteration.current_level.current_step
+        else:
+            _previous_iteration_current_step = self._previous_iteration_current_step(state)
+        if not _previous_iteration_current_step.rhs_evaluated:
+            _previous_iteration_current_step.rhs = \
+                _problem.evaluate(_previous_iteration_current_step.time_point,
+                                  _previous_iteration_current_step.value)
+
+        if not state.current_iteration.on_finest_level:
+            _previous_iteration_previous_step = state.current_iteration.current_level.previous_step
+        else:
+            _previous_iteration_previous_step = self._previous_iteration_previous_step(state)
+        if not _previous_iteration_previous_step.rhs_evaluated:
+            _previous_iteration_previous_step.rhs = \
+                _problem.evaluate(_previous_iteration_previous_step.time_point,
+                                  _previous_iteration_previous_step.value)
+
+        _fas = np.zeros(_previous_iteration_current_step.rhs.shape,
+                        dtype=_previous_iteration_current_step.rhs.dtype)
+        if _previous_iteration_current_step.has_fas_correction():
+            # LOG.debug("   previous iteration current step has FAS")
+            _fas = _previous_iteration_current_step.fas_correction
 
         if problem_has_direct_implicit(_problem, self):
             _sol = _problem.direct_implicit(phis_of_time=[_previous_iteration_previous_step.value,
                                                           _previous_iteration_current_step.value,
                                                           state.previous_step.value],
                                             delta_node=state.current_step.delta_tau,
-                                            delta_step=state.delta_itnerval,
-                                            integral=state.current_step.integral)
+                                            delta_step=state.delta_interval,
+                                            integral=state.current_step.integral,
+                                            fas=_fas,
+                                            core=self)
 
         else:
             # Note: \Delta_t is always 1.0 as it's part of the integral
@@ -67,7 +92,7 @@ class SemiImplicitMlSdcCore(MlSdcSolverCore):
                    - _problem.evaluate(state.current_step.time_point,
                                        _previous_iteration_current_step.value,
                                        partial="impl")) \
-                + state.current_step.integral
+                + state.current_step.integral + _fas
             _func = lambda x_next: \
                 _expl_term \
                 + state.current_step.delta_tau * _problem.evaluate(state.current_step.time_point,
