916258acc64ee00d9684b6f1113fc324efe52388
Torbjörn Klatt <t.klatt@fz-juelich.de>
2014-05-09 12:08:49 +0200

    problems: abstracting problems

--- pypint/problems/i_problem.py
+++ pypint/problems/i_problem.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 """
-
 .. moduleauthor: Torbjörn Klatt <t.klatt@fz-juelich.de>
 """
 import warnings
@@ -16,6 +15,8 @@ from pypint.utilities.logging import LOG
 class IProblem(object):
     """Basic interface for all problems of type :math:`u'(t,\\phi(t))=F(t,\\phi(t))`
     """
+
+    valid_numeric_types = ['i', 'u', 'f', 'c']
 
     def __init__(self, *args, **kwargs):
         """
@@ -35,35 +36,27 @@ class IProblem(object):
             *(optional)*
             String representation of the right hand side function for logging output.
         """
-        self._function = None
-        if "function" in kwargs:
-            self.function = kwargs["function"]
-
-        self._time_start = 0.0
-        if "time_start" in kwargs:
-            self.time_start = kwargs["time_start"]
-
-        self._time_end = 1.0
-        if "time_end" in kwargs:
-            self.time_end = kwargs["time_end"]
+        self._rhs_function_wrt_time = None
+        if 'rhs_function_wrt_time' in kwargs:
+            self.rhs_function_wrt_time = kwargs['rhs_function_wrt_time']
 
         self._numeric_type = np.float
-        if "numeric_type" in kwargs:
-            self.numeric_type = kwargs["numeric_type"]
+        if 'numeric_type' in kwargs:
+            self.numeric_type = kwargs['numeric_type']
 
-        self._dim = kwargs["dim"] if "dim" in kwargs else 1
+        self._dim = kwargs.get('dim', 1)
 
         self._strings = {
-            "rhs": None,
-            "exact": None
+            'rhs': None,
+            'exact': None
         }
-        if "strings" in kwargs:
-            if "rhs" in kwargs["strings"]:
-                self._strings["rhs"] = kwargs["strings"]["rhs"]
+        if 'strings' in kwargs:
+            if 'rhs_wrt_time' in kwargs['strings']:
+                self._strings['rhs_wrt_time'] = kwargs['strings']['rhs_wrt_time']
 
         self._count_rhs_eval = 0
 
-    def evaluate(self, time, phi_of_time, partial=None):
+    def evaluate_wrt_time(self, time, phi_of_time, **kwargs):
         """Evaluates given right hand side at given time and with given time-dependent value.
 
         Parameters
@@ -73,6 +66,7 @@ class IProblem(object):
         phi_of_time : :py:class:`numpy.ndarray`
             Time-dependent data.
         partial : :py:class:`str` or :py:class:`None`
+            *(optional)*
             Specifying whether only a certain part of the problem function should be evaluated.
             E.g. useful for semi-implicit SDC where the imaginary part of the function is explicitly evaluated and
             the real part of the function implicitly.
@@ -89,6 +83,8 @@ class IProblem(object):
         """
         assert_is_instance(time, float, descriptor="Time Point", checking_obj=self)
         assert_is_instance(phi_of_time, np.ndarray, descriptor="Data Vector", checking_obj=self)
+        if kwargs.get('partial') is not None:
+            assert_is_instance(kwargs['partial'], str, descriptor="Partial Descriptor", checking_obj=self)
         self._count_rhs_eval += 1
         return np.zeros(self.dim, dtype=self.numeric_type)
 
@@ -128,18 +124,18 @@ class IProblem(object):
             If the implicit solver did not converged, i.e. the solution object's ``success`` is not :py:class:`True`.
         """
         assert_is_instance(next_x, np.ndarray, descriptor="Initial Guess", checking_obj=self)
-        assert_is_callable(func, descriptor="Function of RHS", checking_obj=self)
+        assert_is_callable(func, descriptor="Function of RHS for Implicit Solver", checking_obj=self)
         sol = find_root(fun=func, x0=next_x, method=method)
         if not sol.success:
             warnings.warn("Implicit solver did not converged.")
-            LOG.debug("sol.x: " + str(sol.x))
-            LOG.error("Implicit solver failed: {:s}".format(sol.message))
+            LOG.debug("sol.x: %s" % sol.x)
+            LOG.error("Implicit solver failed: %s" % sol.message)
         else:
             assert_is_instance(sol.x, np.ndarray, descriptor="Solution", checking_obj=self)
         return sol.x
 
     @property
-    def function(self):
+    def rhs_function_wrt_time(self):
         """Accessor for the right hand side function.
 
         Parameters
@@ -152,12 +148,12 @@ class IProblem(object):
         rhs_function : :py:class:`callable`
             Function of the right hand side.
         """
-        return self._function
+        return self._rhs_function_wrt_time
 
-    @function.setter
-    def function(self, function):
-        assert_is_callable(function, checking_obj=self)
-        self._function = function
+    @rhs_function_wrt_time.setter
+    def rhs_function_wrt_time(self, function):
+        assert_is_callable(function, descriptor="Function of the RHS w.r.t Time", checking_obj=self)
+        self._rhs_function_wrt_time = function
 
     @property
     def rhs_evaluations(self):
@@ -166,46 +162,6 @@ class IProblem(object):
     @rhs_evaluations.deleter
     def rhs_evaluations(self):
         self._count_rhs_eval = 0
-
-    @property
-    def time_start(self):
-        """Accessor for the time interval's start.
-
-        Parameters
-        ----------
-        interval_start : :py:class:`float`
-            Start point of the time interval.
-
-        Returns
-        -------
-        interval_start : :py:class:`float`
-            Start point of the time interval.
-        """
-        return self._time_start
-
-    @time_start.setter
-    def time_start(self, time_start):
-        self._time_start = time_start
-
-    @property
-    def time_end(self):
-        """Accessor for the time interval's end.
-
-        Parameters
-        ----------
-        interval_end : :py:class:`float`
-            End point of the time interval.
-
-        Returns
-        -------
-        interval_end : :py:class:`float`
-            End point of the time interval.
-        """
-        return self._time_end
-
-    @time_end.setter
-    def time_end(self, time_end):
-        self._time_end = time_end
 
     @property
     def numeric_type(self):
@@ -230,8 +186,7 @@ class IProblem(object):
     @numeric_type.setter
     def numeric_type(self, numeric_type):
         numeric_type = np.dtype(numeric_type)
-        _valid_types = ['i', 'u', 'f', 'c']
-        assert_is_in(numeric_type.kind, _valid_types, elem_desc="Numeric Type", list_desc="Valid Types",
+        assert_is_in(numeric_type.kind, IProblem.valid_numeric_types, elem_desc="Numeric Type", list_desc="Valid Types",
                      checking_obj=self)
         self._numeric_type = numeric_type
 
@@ -247,17 +202,15 @@ class IProblem(object):
 
     def print_lines_for_log(self):
         _lines = OrderedDict()
-        if self._strings['rhs'] is not None:
-            _lines['Formula'] = 'u(t, \phi(t)) = %s' % self._strings["rhs"]
-        _lines['Interval'] = '[{:.3f}, {:.3f}]'.format(self.time_start, self.time_end)
+        if self._strings['rhs_wrt_time'] is not None:
+            _lines['Formula w.r.t. Time'] = r"u(t, \phi(t)) = %s" % self._strings['rhs_wrt_time']
         return _lines
 
     def __str__(self):
-        if self._strings["rhs"] is not None:
-            _outstr = r"u'(t,\phi(t))={:s}".format(self._strings["rhs"])
+        if self._strings['rhs_wrt_time'] is not None:
+            _outstr = r"u'(t,\phi(t))=%s" % self._strings['rhs_wrt_time']
         else:
-            _outstr = r"{:s}".format(class_name(self))
-        _outstr += r", t \in [{:.2f}, {:.2f}]".format(self.time_start, self.time_end)
+            _outstr = r"%s" % class_name(self)
         return _outstr
 
 
