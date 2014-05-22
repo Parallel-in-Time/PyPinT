a2022ea2047f210fbace7b064b3dbb3cd39f60ca
Dieter Moser <d.moser@fz-juelich.de>
2014-05-22 15:49:57 +0200

    working impl euler newton krylov strategie

--- examples/sdc_aviles_giga.py
+++ examples/sdc_aviles_giga.py
@@ -13,50 +13,20 @@ from pypint.utilities.threshold_check import ThresholdCheck
 
 problems = [
     # Constant(constant=-1.0),
-    AvilesGiga(epsilon=0.3, n=)
+    AvilesGiga(epsilon=0.1, n=16, time_start=0.0, time_end=1e-4)
 ]
 
 def solve_with_factory(prob, core, num_solvers, num_total_time_steps):
-    thresh = ThresholdCheck(max_threshold=25,
+    thresh = ThresholdCheck(max_threshold=5,
                             min_threshold=1e-7,
-                            conditions=('error', 'error reduction', 'residual', 'iterations'))
-    solvers = sdc_solver_factory(prob, num_solvers, num_total_time_steps, core, threshold=thresh, num_time_steps=2, num_nodes=3)
-
-def solve_parallel(prob, _core):
-    thresh = ThresholdCheck(max_threshold=25,
-                            min_threshold=1e-7,
-                            conditions=('error', 'solution reduction', 'error reduction', 'residual', 'iterations'))
-    comm = ForwardSendingMessaging()
-    solver = ParallelSdc(communicator=comm)
-    comm.link_solvers(previous=comm, next=comm)
-    comm.write_buffer(value=prob.initial_value, time_point=prob.time_start)
-    solver.init(integrator=SdcIntegrator, threshold=thresh, problem=prob, num_time_steps=1, num_nodes=3)
-    sol = solver.run(core=_core, dt=0.5)
-    print(sol)
-
-def solve_parallel_two(prob, _core):
-    thresh = ThresholdCheck(max_threshold=25,
-                            min_threshold=1e-7,
-                            conditions=('error', 'solution reduction', 'error reduction', 'residual', 'iterations'))
-    comm1 = ForwardSendingMessaging()
-    comm2 = ForwardSendingMessaging()
-    solver1 = ParallelSdc(communicator=comm1)
-    solver2 = ParallelSdc(communicator=comm2)
-    comm1.link_solvers(previous=comm2, next=comm2)
-    comm1.write_buffer(value=prob.initial_value, time_point=prob.time_start)
-    comm2.link_solvers(previous=comm1, next=comm1)
-    solver1.init(integrator=SdcIntegrator, threshold=thresh, problem=prob, num_time_steps=1, num_nodes=3)
-    solver2.init(integrator=SdcIntegrator, threshold=thresh, problem=prob, num_time_steps=1, num_nodes=3)
-
-    solver1.run(core=_core, dt=0.25)
-    solver2.run(core=_core, dt=0.25)
-    solver1.run(core=_core, dt=0.25)
-    solver2.run(core=_core, dt=0.25)
+                            conditions=('residual', 'iterations'))
+    solvers = sdc_solver_factory(prob, num_solvers, num_total_time_steps, core, threshold=thresh, num_time_steps=1, num_nodes=3)
 
 for prob in problems:
     for core in [SemiImplicitSdcCore]:
-        solve_with_factory(prob, core, 1, 1)
+        solve_with_factory(prob, core, 1, 10)
         print("RHS Evaluations: %d" % prob.rhs_evaluations)
         del prob.rhs_evaluations
         # solve_parallel(prob, core)
         # solve_parallel_two(prob, core)
+
